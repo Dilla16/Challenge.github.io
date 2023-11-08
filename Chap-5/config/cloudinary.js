d6494453ba9f672253c2909dbd8a06f5b0c1c100
Dilla16 <fadillahanggraini16@gmail.com>
2023-11-08 23:59:53 +0700

    readme

--- Chap-5/config/cloudinary.js
+++
@@ -1,8 +0,0 @@
-const cloudinary = require("cloudinary").v2;
-
-cloudinary.config({
-  cloud_name: "dxwgrdf9s",
-  api_key: "715952786453313",
-  api_secret: "JQSmNj3VBVfyfi_lTq4IkCCTd_A",
-});
-module.exports = cloudinary;
